ab642db7e9a77af212863f454281f057020249b7
Brent Bauman <brentbauman86@gmail.com>
2018-07-17 16:05:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,7 +4,7 @@ class Dog
   attr_reader :id
 
   def initialize(attributes)
-    attributes.each {|key, value| self.send (("#{key}="), value)}
+    attributes.each {|key, value| self.send(("#{key}="), value)}
     self.id ||= nil
   end
 
