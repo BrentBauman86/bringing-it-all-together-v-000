fb6c5e3636a650b69a7c3e2f5e5f66c356bbbc41
Brent Bauman <brentbauman86@gmail.com>
2018-07-17 17:13:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,8 +66,12 @@ end
 
     dog = DB[:conn].execute(sql, name, breed).first
 
-    if dog 
-
+    if dog
+      new_dog = self.new_from_db(dog)
+    else
+      new_dog = self.create({:name => name, :breed => breed})
+    end
+    new_dog
   end
 
   def self.new_from_db(row)
