f1b7f184fa9bc3c21bd225d1dcaf0fa3e7415fe2
Brent Bauman <brentbauman86@gmail.com>
2018-07-17 17:09:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,9 +58,9 @@ end
     end.first
   end
 
-  def self.find_or_create_by
+  def self.find_or_create_by(:name, :breed)
     sql = <<-SQL
-
+      SELECT * FROM dogs WHERE name = ?, breed = ?
     SQL
 
     DB[:conn].execute(sql).map
