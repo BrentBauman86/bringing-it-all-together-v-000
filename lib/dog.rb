32ae053c1d3bf0aa2882df3a82f61048693191f2
Brent Bauman <brentbauman86@gmail.com>
2018-07-17 15:25:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 class Dog
 
-  attr_accessor :name, :breed 
-  attr_reader :id 
+  attr_accessor :name, :breed
+  attr_reader :id
 
   def initialize(name, breed, id=nil)
-    @name = name 
-    @breed = breed 
-    @id = id 
+    @name = name
+    @breed = breed
+    @id = id
   end
 
 
