0e5d03af6594667ef449d2ab6d75267c7088aeb8
Brent Bauman <brentbauman86@gmail.com>
2018-07-17 15:49:56 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,54 +8,54 @@ class Dog
     @id = id
   end
 
-  def self.create_table
-    sql = <<-SQL
-      CREATE TABLE dog
-      id PRIMARY KEY INTEGER
-      VALUES name TEXT,
-      breed TEXT
-    SQL
-
-    DB[:conn].execute(sql)
-  end
-
-  def self.drop_table
-    sql = <<-SQL
-      DROP TABLE dog
-    SQL
-
-    DB[:conn].execute(sql)
-  end
-
-  def save
-    if self.id
-      self.update
-    else
-    sql = <<-SQL
-
-    SQL
-
-    DB[:conn].execute(sql, self.name, self.breed)
-  end
-end
-
-  def self.create
-    sql = <<-SQL
-
-    SQL
-
-    DB[:conn].execute(sql)
-  end
-
-  def self.find_or_create_by
-
-  end
-
-  def self.new_from_db
-
-  end
-
-  def update
-
-  end
+#   def self.create_table
+#     sql = <<-SQL
+#       CREATE TABLE dog
+#       id PRIMARY KEY INTEGER
+#       VALUES name TEXT,
+#       breed TEXT
+#     SQL
+# 
+#     DB[:conn].execute(sql)
+#   end
+# 
+#   def self.drop_table
+#     sql = <<-SQL
+#       DROP TABLE dog
+#     SQL
+# 
+#     DB[:conn].execute(sql)
+#   end
+# 
+#   def save
+#     if self.id
+#       self.update
+#     else
+#     sql = <<-SQL
+# 
+#     SQL
+# 
+#     DB[:conn].execute(sql, self.name, self.breed)
+#   end
+# end
+# 
+#   def self.create
+#     sql = <<-SQL
+# 
+#     SQL
+# 
+#     DB[:conn].execute(sql)
+#   end
+# 
+#   def self.find_or_create_by
+# 
+#   end
+# 
+#   def self.new_from_db
+# 
+#   end
+# 
+#   def update
+# 
+#   end
 end
