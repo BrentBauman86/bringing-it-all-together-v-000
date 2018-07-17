066a8b4c7818c15abdc4fe7262e34a24d7803f3d
Brent Bauman <brentbauman86@gmail.com>
2018-07-17 15:49:59 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -15,47 +15,47 @@ class Dog
 #       VALUES name TEXT,
 #       breed TEXT
 #     SQL
-# 
+#
 #     DB[:conn].execute(sql)
 #   end
-# 
+#
 #   def self.drop_table
 #     sql = <<-SQL
 #       DROP TABLE dog
 #     SQL
-# 
+#
 #     DB[:conn].execute(sql)
 #   end
-# 
+#
 #   def save
 #     if self.id
 #       self.update
 #     else
 #     sql = <<-SQL
-# 
+#
 #     SQL
-# 
+#
 #     DB[:conn].execute(sql, self.name, self.breed)
 #   end
 # end
-# 
+#
 #   def self.create
 #     sql = <<-SQL
-# 
+#
 #     SQL
-# 
+#
 #     DB[:conn].execute(sql)
 #   end
-# 
+#
 #   def self.find_or_create_by
-# 
+#
 #   end
-# 
+#
 #   def self.new_from_db
-# 
+#
 #   end
-# 
+#
 #   def update
-# 
+#
 #   end
 end
