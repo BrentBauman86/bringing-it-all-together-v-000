dd20a48fe4d4c6c9bb62e6e43b625e568c4a2a61
Brent Bauman <brentbauman86@gmail.com>
2018-07-17 16:16:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,6 +27,9 @@ class Dog
   end
 
   def save
+    if self.id
+      self.update
+    else
     sql = <<-SQL
       INSERT INTO dogs (name, breed)
       VALUES (?, ?)
@@ -37,6 +40,7 @@ class Dog
 
     self
   end
+end
 #
 #   def self.create
 #     sql = <<-SQL
